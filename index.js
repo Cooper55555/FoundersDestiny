8b445f76dfe47dec7992d871324f1d99e1381c16
Cooper55555 <milanvelthaak1@gmail.com>
2025-01-22 14:14:24 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const cards = [
     { name: '04/120', image: 'Pokemon set/4.png', description: 'Cacnea' },
     { name: '05/120', image: 'Pokemon set/5.png', description: 'Cacturne' },
     { name: '06/120', image: 'Pokemon set/6.png', description: 'Tropius' },
+    { name: '07/120', image: 'Pokemon set/7.png', description: 'Sunflora' },
     { name: '08/120', image: 'Pokemon set/8.png', description: 'Tangela' },
     { name: '09/120', image: 'Pokemon set/9.png', description: 'Tangrowth' },
     { name: '11/120', image: 'Pokemon set/11.png', description: 'Torchic' },
     { name: '12/120', image: 'Pokemon set/12.png', description: 'Combusken' },
     { name: '13/120', image: 'Pokemon set/13.png', description: 'Blaziken' },
     { name: '21/120', image: 'Pokemon set/21.png', description: 'Piplup' },
+    { name: '50/120', image: 'Pokemon set/50.png', description: 'Croagunk' },
+    { name: '51/120', image: 'Pokemon set/51.png', description: 'Toxicroak' },
+    { name: '52/120', image: 'Pokemon set/52.png', description: 'Riolu' },
+    { name: '53/120', image: 'Pokemon set/53.png', description: 'Lucario' },
 ];
 
 function displayCards() {
