1807f31f94684800d0e35c0ad2c1bddd395fbbce
Cooper55555 <milanvelthaak1@gmail.com>
2025-01-22 08:32:13 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const cards = [
     { name: '01/120', image: 'Pokemon set/1.png', description: 'Treecko' },
+    { name: '02/120', image: 'Pokemon set/2.png', description: 'Grovyle' },
+    { name: '03/120', image: 'Pokemon set/3.png', description: 'Sceptile' },
+    { name: '04/120', image: 'Pokemon set/4.png', description: 'Cacnea' },
+    { name: '05/120', image: 'Pokemon set/5.png', description: 'Cacturne' },
+    { name: '06/120', image: 'Pokemon set/6.png', description: 'Tropius' },
+    { name: '08/120', image: 'Pokemon set/8.png', description: 'Tangela' },
+    { name: '09/120', image: 'Pokemon set/9.png', description: 'Tangrowth' },
+    { name: '11/120', image: 'Pokemon set/11.png', description: 'Torchic' },
+    { name: '12/120', image: 'Pokemon set/12.png', description: 'Combusken' },
     { name: '13/120', image: 'Pokemon set/13.png', description: 'Blaziken' },
     { name: '21/120', image: 'Pokemon set/21.png', description: 'Piplup' },
 ];
